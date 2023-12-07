9b318e8c4e96a1f0383b0e2781b9236d842e1aae
Trisztan <trisztanfarkas123@gmail.com>
2023-12-07 18:05:55 +0100

    Add variables and conditional statement
    Conditional statement allows to playerSelection answer be case-insensitive

--- main.js
+++ main.js
@@ -16,7 +16,16 @@ function getComputerChoices() {
 // 
 
 let playerSelection = prompt('What is your move?', '');
-if (playerSelection.charAt(0) == p || playerSelection.charAt(0) == P) {
-    
+const paper = 1;
+const rock = 2;
+const scissors = 3;
+if (playerSelection.charAt(0) == 'p' || playerSelection.charAt(0) == 'P') {
+    playerSelection = paper;
+} else if (playerSelection.charAt(0) == 'r' || playerSelection.charAt(0) == 'R') {
+    playerSelection = rock;
+} else if (playerSelection.charAt(0) == 's' || playerSelection.charAt(0) == 'S') {
+    playerSelection = scissors;
 }
+
+console.log(playerSelection);
 
